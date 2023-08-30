da38d0c5d26e777d1b6548f985a5b206eeea649e
cheesepizza453 <71148177+cheesepizza453@users.noreply.github.com>
2023-08-30 20:15:31 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var answer = 0;
+    // if( n <= 7 ){
+    //     return 1;
+    // }
+    // answer = parseInt(n / 7 + 1);
+    // return answer;
+    return Math.ceil(n/7)
+}
